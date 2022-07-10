ca05b5c6f2bd9b60dc636b55b906d50dcad35a19
wuxins <18676780050@sina.cn>
2022-07-10 09:11:48 +0800

    create table change

--- sql/02_create_table.sql
+++ sql/02_create_table.sql
@@ -1,26 +1,42 @@
 use api_gateway;
 
-drop table if exists `api`;
-CREATE TABLE `api`
+
+drop table if exists `service`;
+CREATE TABLE `service`
 (
     `id`           bigint       NOT NULL AUTO_INCREMENT COMMENT 'primary key',
-    `name`         varchar(32)  NOT NULL COMMENT 'api name',
-    `api_code`     varchar(32)  NOT NULL COMMENT 'api code',
-    `method`       varchar(8)   NOT NULL COMMENT 'api request method',
-    `src_url`      varchar(64)  NOT NULL COMMENT 'api request origin url',
-    `des_url`      varchar(128) NOT NULL COMMENT 'api proxy forwarding url',
-    `description`  varchar(2048) DEFAULT NULL COMMENT 'api introduction',
-    `created_time` datetime      DEFAULT CURRENT_TIMESTAMP COMMENT 'record created time',
-    `updated_time` datetime      DEFAULT CURRENT_TIMESTAMP COMMENT 'record updated time',
-    `created_by`   varchar(255)  DEFAULT 'system' COMMENT 'who created the record',
-    `updated_by`   varchar(255)  DEFAULT 'system' COMMENT 'who updated the record',
-    `is_deleted`   char(1)       DEFAULT 'N' COMMENT 'logical delete identifier(Y-effective,N-ineffective)',
+    `name`         varchar(32)  NOT NULL COMMENT 'service name',
+    `service_code` varchar(32)  NOT NULL COMMENT 'service code',
+    `addresses`    varchar(512) NOT NULL COMMENT 'service addresses',
+    `created_time` datetime     DEFAULT CURRENT_TIMESTAMP COMMENT 'record created time',
+    `updated_time` datetime     DEFAULT CURRENT_TIMESTAMP COMMENT 'record updated time',
+    `created_by`   varchar(255) DEFAULT 'system' COMMENT 'who created the record',
+    `updated_by`   varchar(255) DEFAULT 'system' COMMENT 'who updated the record',
+    `is_deleted`   char(1)      DEFAULT 'N' COMMENT 'logical delete identifier(Y-effective,N-ineffective)',
     PRIMARY KEY (`id`) USING BTREE,
-    KEY `idx_code` (`api_code`)
+    KEY `idx_code` (`service_code`)
 ) ENGINE = InnoDB
   AUTO_INCREMENT = 100000000
   DEFAULT CHARSET = utf8
-  ROW_FORMAT = COMPACT COMMENT ='Api define';
+  ROW_FORMAT = COMPACT COMMENT ='Api service define';
+
+drop table if exists `tenant`;
+CREATE TABLE `tenant`
+(
+    `id`           bigint      NOT NULL AUTO_INCREMENT COMMENT 'primary key',
+    `name`         varchar(32) NOT NULL COMMENT 'api tenant name',
+    `tenant_code`  varchar(32) NOT NULL COMMENT 'api tenant code',
+    `created_time` datetime     DEFAULT CURRENT_TIMESTAMP COMMENT 'record created time',
+    `updated_time` datetime     DEFAULT CURRENT_TIMESTAMP COMMENT 'record updated time',
+    `created_by`   varchar(255) DEFAULT 'system' COMMENT 'who created the record',
+    `updated_by`   varchar(255) DEFAULT 'system' COMMENT 'who updated the record',
+    `is_deleted`   char(1)      DEFAULT 'N' COMMENT 'logical delete identifier(Y-effective,N-ineffective)',
+    PRIMARY KEY (`id`) USING BTREE,
+    KEY `idx_code` (`tenant_code`)
+) ENGINE = InnoDB
+  AUTO_INCREMENT = 100000000
+  DEFAULT CHARSET = utf8
+  ROW_FORMAT = COMPACT COMMENT ='Api tenant or invoker';
 
 drop table if exists `group`;
 CREATE TABLE `group`
@@ -40,23 +56,31 @@ CREATE TABLE `group`
   DEFAULT CHARSET = utf8
   ROW_FORMAT = COMPACT COMMENT ='Api classification';
 
-drop table if exists `tenant`;
-CREATE TABLE `tenant`
+
+drop table if exists `api`;
+CREATE TABLE `api`
 (
-    `id`           bigint      NOT NULL AUTO_INCREMENT COMMENT 'primary key',
-    `name`         varchar(32) NOT NULL COMMENT 'api tenant name',
-    `tenant_code`  varchar(32) NOT NULL COMMENT 'api tenant code',
-    `created_time` datetime     DEFAULT CURRENT_TIMESTAMP COMMENT 'record created time',
-    `updated_time` datetime     DEFAULT CURRENT_TIMESTAMP COMMENT 'record updated time',
-    `created_by`   varchar(255) DEFAULT 'system' COMMENT 'who created the record',
-    `updated_by`   varchar(255) DEFAULT 'system' COMMENT 'who updated the record',
-    `is_deleted`   char(1)      DEFAULT 'N' COMMENT 'logical delete identifier(Y-effective,N-ineffective)',
+    `id`           bigint       NOT NULL AUTO_INCREMENT COMMENT 'primary key',
+    `name`         varchar(32)  NOT NULL COMMENT 'api name',
+    `api_code`     varchar(32)  NOT NULL COMMENT 'api code',
+    `method`       varchar(8)   NOT NULL COMMENT 'api request method',
+    `service_code` varchar(8)   NOT NULL COMMENT 'api service code',
+    `src_url`      varchar(64)  NOT NULL COMMENT 'api request origin url',
+    `des_url`      varchar(128) NOT NULL COMMENT 'api proxy forwarding url',
+    `maintainer`   varchar(64)  NOT NULL COMMENT 'api who maintain',
+    `description`  varchar(2048) DEFAULT NULL COMMENT 'api introduction',
+    `created_time` datetime      DEFAULT CURRENT_TIMESTAMP COMMENT 'record created time',
+    `updated_time` datetime      DEFAULT CURRENT_TIMESTAMP COMMENT 'record updated time',
+    `created_by`   varchar(255)  DEFAULT 'system' COMMENT 'who created the record',
+    `updated_by`   varchar(255)  DEFAULT 'system' COMMENT 'who updated the record',
+    `is_deleted`   char(1)       DEFAULT 'N' COMMENT 'logical delete identifier(Y-effective,N-ineffective)',
     PRIMARY KEY (`id`) USING BTREE,
-    KEY `idx_code` (`tenant_code`)
+    KEY `idx_code` (`api_code`)
 ) ENGINE = InnoDB
   AUTO_INCREMENT = 100000000
   DEFAULT CHARSET = utf8
-  ROW_FORMAT = COMPACT COMMENT ='Api tenant or invoker';
+  ROW_FORMAT = COMPACT COMMENT ='Api define';
+
 
 drop table if exists `api_group`;
 CREATE TABLE `api_group`
@@ -100,6 +124,7 @@ CREATE TABLE `api_version`
     `id`              bigint       NOT NULL AUTO_INCREMENT COMMENT 'primary key',
     `api_code`        varchar(32)  NOT NULL COMMENT 'api code',
     `env`             varchar(32)  NOT NULL COMMENT 'api env',
+    `tenant_code`     varchar(32)  NOT NULL COMMENT 'api tenant, if (*) means common rules, other wise special tenant',
     `upstream_host`   varchar(128) NOT NULL COMMENT 'api upstream server host',
     `need_rate_limit` char(1)       DEFAULT 'N' COMMENT 'rate limit switch',
     `rate_limit`      int           DEFAULT NULL COMMENT 'rate limit size',
@@ -118,3 +143,22 @@ CREATE TABLE `api_version`
   AUTO_INCREMENT = 100000000
   DEFAULT CHARSET = utf8
   ROW_FORMAT = COMPACT COMMENT ='Api configuration';
+
+drop table if exists `api_version_changelog`;
+CREATE TABLE `api_version_changelog`
+(
+    `id`           bigint      NOT NULL AUTO_INCREMENT COMMENT 'primary key',
+    `api_id`       varchar(32) NOT NULL COMMENT 'api code',
+    `before`       varchar(32) NOT NULL COMMENT 'before content',
+    `after`        varchar(32) NOT NULL COMMENT 'after content',
+    `tenant_code`  varchar(32) NOT NULL COMMENT 'api tenant, if (*) means common rules, other wise special tenant',
+    `created_time` datetime     DEFAULT CURRENT_TIMESTAMP COMMENT 'record created time',
+    `updated_time` datetime     DEFAULT CURRENT_TIMESTAMP COMMENT 'record updated time',
+    `created_by`   varchar(255) DEFAULT 'system' COMMENT 'who created the record',
+    `updated_by`   varchar(255) DEFAULT 'system' COMMENT 'who updated the record',
+    `is_deleted`   char(1)      DEFAULT 'N' COMMENT 'logical delete identifier(Y-effective,N-ineffective)',
+    PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB
+  AUTO_INCREMENT = 100000000
+  DEFAULT CHARSET = utf8
+  ROW_FORMAT = COMPACT COMMENT ='Api version change log';

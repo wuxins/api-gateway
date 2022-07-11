bb0152a461a7280da95433aa4229c02847d5be81
wuxins <18676780050@sina.cn>
2022-07-11 14:26:03 +0800

    utils

---
+++ utils/httputils.go
@@ -0,0 +1,54 @@
+package utils
+
+import (
+	"bytes"
+	"github.com/wuxins/api-gateway/common"
+	"github.com/wuxins/api-gateway/monitor"
+	"github.com/wuxins/api-gateway/regularpath"
+	"io/ioutil"
+	"net/http"
+	"strconv"
+	"strings"
+	"time"
+)
+
+func WriteHttpResponse(w http.ResponseWriter, status int, body string, r *http.Request, regularPath *regularpath.RegularPath) {
+
+	value := r.Header[common.HeaderContentEncoding]
+	IsGzipEncode := value != nil && strings.EqualFold(value[0], "gzip")
+	var bodyBytes []byte
+	if IsGzipEncode {
+		bodyBytes = common.EncodeGzipBytes(common.StringToBytes(body))
+	} else {
+		bodyBytes = common.StringToBytes(body)
+	}
+	w.WriteHeader(status)
+	if regularPath.NeedMonitor {
+		requestBody, _ := ioutil.ReadAll(r.Body)
+		r.Body = ioutil.NopCloser(bytes.NewBuffer(requestBody))
+		requestStartTime, _ := strconv.ParseInt(r.Header[common.HeaderRequestTime][0], 10, 64)
+		now := time.Now()
+		monitor.Report(monitor.Event{
+			Metric:     monitor.MetricApi,
+			MetricType: monitor.MetricApiAccLog,
+			Time:       now.Format(common.DateFormatMs),
+			Key:        r.Header[common.HeaderRequestId][0],
+			Content: monitor.ApiTransportMetric{
+				Url:        r.URL.Path,
+				Method:     r.Method,
+				ReqHeader:  r.Header,
+				ReqBody:    string(requestBody),
+				Status:     status,
+				RespHeader: w.Header(),
+				RespBody:   body,
+				StartTime:  requestStartTime,
+				EndTime:    common.UnixMilliseconds(now),
+				Cost:       common.UnixMilliseconds(now) - requestStartTime,
+			},
+		})
+	}
+	_, err := w.Write(bodyBytes)
+	if err != nil {
+		return
+	}
+}

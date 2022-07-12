fb0ef9fa8154973f4a171f504ad8d72767c47132
wuxins <18676780050@sina.cn>
2022-07-12 20:41:47 +0800

    Create tenant_check_plugin.go

---
+++ routing/tenant_check_plugin.go
@@ -0,0 +1,34 @@
+package routing
+
+import (
+	"github.com/wuxins/api-gateway/common"
+	"github.com/wuxins/api-gateway/utils"
+	"net/http"
+)
+
+func TenantCheckPlugin() func(c *RouterContext) {
+
+	return func(c *RouterContext) {
+
+		w := c.Rw
+		r := c.Req
+		regularPath := c.RegularPath
+		tenant := r.Header[common.HeaderTenant]
+		supportedTenants := regularPath.Tenants
+		isTenantSupport := false
+		if tenant != nil && len(supportedTenants) > 0 {
+			for _, item := range supportedTenants {
+				if tenant[0] == item.TenantCode {
+					isTenantSupport = true
+				}
+			}
+		}
+
+		if !isTenantSupport {
+			utils.WriteHttpResponse(w, http.StatusForbidden, common.ChannelUnsupportedMsg, r, true)
+			c.Abort()
+			return
+		}
+		c.Next()
+	}
+}

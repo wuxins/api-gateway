f8960e9e3d92decae83babf305f6b0ddf5e1ee9f
wuxins <18676780050@sina.cn>
2022-08-05 17:55:39 +0800

    Update OauthToken.go

--- dto/OauthToken.go
+++ dto/OauthToken.go
@@ -1,11 +1,11 @@
 package dto
 
-type TokensInput struct {
+type TokensRequest struct {
 	GrantType string `json:"grant_type" form:"grant_type"`
 	Scope     string `json:"scope" form:"scope"`
 }
 
-type TokensOutput struct {
+type TokensResponse struct {
 	AccessToken string `json:"access_token" form:"access_token"`
 	ExpiresIn   int    `json:"expires_in" form:"expires_in"`
 	TokenType   string `json:"token_type" form:"token_type"`

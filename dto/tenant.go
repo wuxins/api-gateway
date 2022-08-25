1a0976311767e2bbfadaedca297f25534229730a
wuxins <18676780050@sina.cn>
2022-08-25 13:48:10 +0800

    Update tenant.go

--- dto/tenant.go
+++ dto/tenant.go
@@ -1,9 +1,11 @@
 package dto
 
 type Tenant struct {
-	Name           string `gorm:"name"`
-	TenantCode     string `gorm:"tenant_code"`
-	NeedApiAuth    string `gorm:"need_api_auth"`
-	ApiAuthContent string `gorm:"api_auth_content"`
-	ApiAuth        ApiAuth
+	Name               string `gorm:"name"`
+	TenantCode         string `gorm:"tenant_code"`
+	NeedApiAuth        string `gorm:"need_api_auth"`
+	ApiAuthContent     string `gorm:"api_auth_content"`
+	ApiAuth            ApiAuth
+	IgnoreHeaderParams string `gorm:"ignore_header_params"`
+	IgnoreQueryParams  string `gorm:"ignore_query_params"`
 }

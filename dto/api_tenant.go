bbb8ac423a28d15fa13dbc4a3c8013455e3c43d5
wuxins <18676780050@sina.cn>
2022-08-25 13:47:36 +0800

    Update api_tenant.go

--- dto/api_tenant.go
+++ dto/api_tenant.go
@@ -1,10 +1,12 @@
 package dto
 
 type ApiTenant struct {
-	ApiCode        string `gorm:"api_code"`
-	Name           string `gorm:"name"`
-	TenantCode     string `gorm:"tenant_code"`
-	NeedApiAuth    string `gorm:"need_api_auth"`
-	ApiAuthContent string `gorm:"api_auth_content"`
-	ApiAuth        ApiAuth
+	ApiCode            string `gorm:"api_code"`
+	Name               string `gorm:"name"`
+	TenantCode         string `gorm:"tenant_code"`
+	NeedApiAuth        string `gorm:"need_api_auth"`
+	ApiAuthContent     string `gorm:"api_auth_content"`
+	ApiAuth            ApiAuth
+	IgnoreHeaderParams string `gorm:"ignore_header_params"`
+	IgnoreQueryParams  string `gorm:"ignore_query_params"`
 }

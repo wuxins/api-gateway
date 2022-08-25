225aed734398758a97bd0e3bba19a501ff5fe5f6
wuxins <18676780050@sina.cn>
2022-08-25 13:44:56 +0800

    Update const.go

--- common/const.go
+++ common/const.go
@@ -52,6 +52,8 @@ SELECT
 	av.need_monitor,
 	av.need_fallback,
 	av.fallback,
+	av.ignore_header_params,
+	av.ignore_query_params,
 	se.address,
 	se.need_gray,
 	se.gray_content
@@ -69,7 +71,7 @@ WHERE
 	AND se.is_deleted = 'N'
 */
 
-var ApiSql = "SELECT\n\ta.id,\n\ta.NAME,\n\ta.api_code,\n\ta.method,\n\ta.src_url,\n\ta.des_url,\n\tav.read_timeout,\n\tav.need_rate_limit,\n\tav.rate_limit,\n\tav.need_monitor,\n\tav.need_fallback,\n\tav.fallback,\n\tse.address,\n\tse.need_gray,\n\tse.gray_content\nFROM\n\tapi_env av\n\tJOIN api a ON a.api_code = av.api_code\n\tAND av.env = ?\n\tJOIN upstream_service s ON s.service_code = a.service_code\n\tJOIN upstream_service_env se ON se.service_code = s.service_code\n\tAND se.env = ?\nWHERE\n\tav.is_deleted = 'N'\n\tAND a.is_deleted = 'N'\n\tAND s.is_deleted = 'N'\n\tAND se.is_deleted = 'N'"
+var ApiSql = "SELECT\n\ta.id,\n\ta.NAME,\n\ta.api_code,\n\ta.method,\n\ta.src_url,\n\ta.des_url,\n\tav.read_timeout,\n\tav.need_rate_limit,\n\tav.rate_limit,\n\tav.need_monitor,\n\tav.need_fallback,\n\tav.fallback,\n\tav.ignore_header_params,\n\tav.ignore_query_params,\n\tse.address,\n\tse.need_gray,\n\tse.gray_content\nFROM\n\tapi_env av\n\tJOIN api a ON a.api_code = av.api_code\n\tAND av.env = ?\n\tJOIN upstream_service s ON s.service_code = a.service_code\n\tJOIN upstream_service_env se ON se.service_code = s.service_code\n\tAND se.env = ?\nWHERE\n\tav.is_deleted = 'N'\n\tAND a.is_deleted = 'N'\n\tAND s.is_deleted = 'N'\n\tAND se.is_deleted = 'N'"
 
 /**
 SELECT
@@ -77,7 +79,9 @@ SELECT
 	t.NAME,
 	t.tenant_code,
 	te.need_api_auth,
-	te.api_auth_content
+	te.api_auth_content,
+	te.ignore_header_params,
+	te.ignore_query_params
 FROM
 	tenant t,
 	api_tenant apt,
@@ -91,14 +95,16 @@ WHERE
 	AND te.env = ?
 */
 
-var ApiTenantSql = "SELECT\n\tapt.api_code,\n\tt.NAME,\n\tt.tenant_code,\n\tte.need_api_auth,\n\tte.api_auth_content\nFROM\n\ttenant t,\n\tapi_tenant apt,\n\ttenant_env te\nWHERE\n\tt.tenant_code = apt.tenant_code\n\tAND apt.tenant_code = te.tenant_code\n\tAND t.is_deleted = 'N'\n\tAND apt.is_deleted = 'N'\n\tAND te.is_deleted = 'N'\n\tAND te.env = ?"
+var ApiTenantSql = "SELECT\n\tapt.api_code,\n\tt.NAME,\n\tt.tenant_code,\n\tte.need_api_auth,\n\tte.api_auth_content,\n\tte.ignore_header_params,\n\tte.ignore_query_params\nFROM\n\ttenant t,\n\tapi_tenant apt,\n\ttenant_env te\nWHERE\n\tt.tenant_code = apt.tenant_code\n\tAND apt.tenant_code = te.tenant_code\n\tAND t.is_deleted = 'N'\n\tAND apt.is_deleted = 'N'\n\tAND te.is_deleted = 'N'\n\tAND te.env = ?"
 
 /**
 SELECT
 	t.NAME,
 	t.tenant_code,
 	te.need_api_auth,
-	te.api_auth_content
+	te.api_auth_content,
+	te.ignore_header_params,
+	te.ignore_query_params
 FROM
 	tenant t
 	JOIN tenant_env te ON t.tenant_code = te.tenant_code
@@ -108,4 +114,4 @@ WHERE
 	AND t.is_deleted = 'N'
 */
 
-var TenantSql = "SELECT\n\tt.NAME,\n\tt.tenant_code,\n\tte.need_api_auth,\n\tte.api_auth_content\nFROM\n\ttenant t\n\tJOIN tenant_env te ON t.tenant_code = te.tenant_code\n\tAND te.env = ?\nWHERE\n\tte.is_deleted = 'N'\n\tAND t.is_deleted = 'N'"
+var TenantSql = "SELECT\n\tt.NAME,\n\tt.tenant_code,\n\tte.need_api_auth,\n\tte.api_auth_content,\n\tte.ignore_header_params,\n\tte.ignore_query_params\nFROM\n\ttenant t\n\tJOIN tenant_env te ON t.tenant_code = te.tenant_code\n\tAND te.env = ?\nWHERE\n\tte.is_deleted = 'N'\n\tAND t.is_deleted = 'N'"

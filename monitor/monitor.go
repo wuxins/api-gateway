0de640f09134fd6f27d23e5faf659da87f9bd1f7
wuxins <18676780050@sina.cn>
2023-05-31 17:13:01 +0800

    Update monitor.go

--- monitor/monitor.go
+++ monitor/monitor.go
@@ -1,6 +1,8 @@
 package monitor
 
 import (
+	"bytes"
+	"github.com/bytedance/sonic"
 	"github.com/sirupsen/logrus"
 	"github.com/wuxins/api-gateway/config"
 	"gopkg.in/natefinch/lumberjack.v2"
@@ -9,19 +11,49 @@ import (
 var log *logrus.Logger
 
 func Init(monitor config.Monitor) {
+
+	filename := "monitor.log"
+	maxSize := 50
+	maxBackups := 100
+	maxAge := 30
+	if len(monitor.Filename) > 0 {
+		filename = monitor.Filename
+	}
+	if monitor.RotateMaxSize > 0 {
+		maxSize = monitor.RotateMaxSize
+	}
+	if monitor.RotateMaxBackups > 0 {
+		maxBackups = monitor.RotateMaxBackups
+	}
+	if monitor.RotateMaxAge > 0 {
+		maxAge = monitor.RotateMaxAge
+	}
+
 	log = logrus.New()
-	log.SetFormatter(&logrus.JSONFormatter{
-		DisableTimestamp: true,
-	})
+	log.SetFormatter(&Formatter{})
 	log.SetOutput(&lumberjack.Logger{
-		Filename:   "monitor.log",
-		MaxSize:    100,
-		MaxBackups: 100,
-		MaxAge:     3,
-		Compress:   true,
+		Filename:   filename,
+		MaxSize:    maxSize,
+		MaxBackups: maxBackups,
+		MaxAge:     maxAge,
+		Compress:   monitor.RotateCompress,
 	})
 }
 
 func Report(event Event) {
-	log.Info(event)
+	output, _ := sonic.Marshal(&event)
+	log.Info(string(output))
+}
+
+type Formatter struct{}
+
+func (m *Formatter) Format(entry *logrus.Entry) ([]byte, error) {
+	var b *bytes.Buffer
+	if entry.Buffer != nil {
+		b = entry.Buffer
+	} else {
+		b = &bytes.Buffer{}
+	}
+	b.WriteString(entry.Message + "\n")
+	return b.Bytes(), nil
 }

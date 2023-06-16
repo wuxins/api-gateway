3d0d3bf40620adfbf2c19521b4af22f4d84af06f
wuxins <18676780050@sina.cn>
2023-06-16 18:32:55 +0800

    Update gray_strategy_plugin.go

--- routing/gray_strategy_plugin.go
+++ routing/gray_strategy_plugin.go
@@ -1,6 +1,8 @@
 package routing
 
 import (
+	"github.com/bytedance/sonic"
+	"github.com/wuxins/api-gateway/dto"
 	"math/rand"
 	"time"
 )
@@ -20,28 +22,33 @@ func GrayStrategyPlugin() func(c *RouterContext) {
 
 		grayStrategy := regularPath.GrayRule
 		hitGray := false
-		featureTagMap := grayStrategy.FeatureTagMap
-		if grayStrategy.Mode == 0 { // feature
-			if len(featureTagMap) > 0 {
-				for key, value := range featureTagMap {
-					if r.Header[key] != nil && r.Header[key][0] == value {
-						hitGray = true
-						break
+		if r.Header[grayStrategy.HeaderPassTag] != nil && r.Header[grayStrategy.HeaderPassTag][0] == "Y" {
+			hitGray = true
+		} else {
+			featureContent := grayStrategy.FeatureContent
+			if grayStrategy.Mode == 0 { // feature
+				if len(featureContent) > 0 {
+					featureContentObj := dto.FeatureContent{}
+					_ = sonic.UnmarshalString(featureContent, &featureContentObj)
+
+					for key, value := range featureContentObj.HeaderTagMap {
+						if r.Header[key] != nil && r.Header[key][0] == value {
+							hitGray = true
+							break
+						}
 					}
 				}
-			}
-		} else { // scale
-			rand.Seed(time.Now().UnixNano())
-			if rand.Intn(100) <= grayStrategy.ScaleRate {
-				hitGray = true
+			} else { // scale+
+				rand.Seed(time.Now().UnixNano())
+				if rand.Intn(100) <= grayStrategy.ScaleRate {
+					hitGray = true
+				}
 			}
 		}
 
 		if hitGray {
 			r.Header.Set(grayStrategy.HeaderPassTag, "Y")
-			if "N" == grayStrategy.Global { // service gray
-				c.RequestUpstreamAddress = regularPath.GrayAddress
-			}
+			c.RequestUpstreamAddress = regularPath.GrayAddress
 		}
 
 		c.Next()
